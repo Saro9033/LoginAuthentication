2a3b399d19420779b0b5d15028d89792c66c58a2
Saro9033 <ssaravanan2310@gmail.com>
2023-09-29 14:33:51 +0530

    update foot

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center py-1 text-light' style={{background:'black'}}>
+    <div className='text-center py-1 text-light' style={{background:'black', position:'fixed',width:'100%', bottom:'0'}}>
         <p> Copyright 2023</p>
     </div>
   )
